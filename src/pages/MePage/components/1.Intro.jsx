02b6ed9c8e1c6db6a94b849d118ed4a8f2f7d75e
John Zhou <itszhoudoh@gmail.com>
2023-03-19 10:55:28 -0700

    small optimizations

--- src/pages/MePage/components/1.Intro.jsx
+++ src/pages/MePage/components/1.Intro.jsx
@@ -1,18 +1,18 @@
 import '../../../styles/me.css'
 
-const date = new Date();
-const time = date.getHours();
+const date = new Date()
+const time = date.getHours()
 
 function Intro() {
     return (
         <div className='header'>
             <div className='top-block'>
                 <div className="title-small"> Good 
-                    {time >= 12 ? time >= 16 ? " Evening": " Afternoon": " Morning"}! My name is </div>
+                    {time >= 12 ? time >= 16 ? <span className="evening"> Evening!</span>: <span className="afternoon"> Afternoon!</span>: <span className="morning"> Morning!</span>} My name is </div>
                     <a className="me-link-big" href="https://www.linkedin.com/in/jjzee" target="_blank"> JOHN ZHOU</a> <br></br>
                     I'm a SoCal-based perfectionist with a love of collaboration.  <br></br>
                     <br></br>
-                    I can <a className='title-small me-link' href="https://www.instagram.com/jozhoooo/" target="_blank">illustrate</a>  <br></br>
+                    I <a className='title-small me-link' href="https://www.instagram.com/jozhoooo/" target="_blank">draw</a> things I like <br></br>
                     I want to <a className='title-small me-link' href="https://github.com/jaayzee" target="_blank">develop</a> stuff  <br></br>
                     I <a className='title-small me-link' href="https://steamcommunity.com/id/belloga/" target="_blank">game</a> way too much
                     <br></br>
