1268e02729b8f5b101d1c34d58529c5f2781a07e
John Zhou <itszhoudoh@gmail.com>
2023-03-19 08:14:53 -0700

    test2

--- src/pages/MePage/components/1.Intro.jsx
+++ src/pages/MePage/components/1.Intro.jsx
@@ -10,7 +10,7 @@ function Intro() {
                 <div className="title-small"> Good 
                     {time >= 12 ? time >= 16 ? " Evening": " Afternoon": " Morning"}! My name is </div>
                     <a className="me-link-big" href="https://www.linkedin.com/in/jjzee" target="_blank"> JOHN ZHOU</a> <br></br>
-                    I'm a SoTEST-based perfectionist with a love of collaboration.  <br></br>
+                    I'm a SoCal-based perfectionist with a love of collaboration.  <br></br>
                     <br></br>
                     I can <a className='title-small me-link' href="https://www.instagram.com/jozhoooo/" target="_blank">illustrate</a>  <br></br>
                     I want to <a className='title-small me-link' href="https://github.com/jaayzee" target="_blank">develop</a> stuff  <br></br>
